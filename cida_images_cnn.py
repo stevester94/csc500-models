102f86989752abb6a2db9f132a8b39b24e671132
Steven Mackey <stevester94@gmail.com>
2021-09-28 22:59:56 -0700

    training, but exploding domain issue still persists

--- cida_images_cnn.py
+++ cida_images_cnn.py
@@ -3,6 +3,8 @@
 import torch.nn as nn
 import torch
 import torch.nn.functional as F
+import torch.optim.lr_scheduler as lr_scheduler
+from torch.autograd import Function
 
 
 
@@ -128,7 +130,6 @@ class EncoderSTN(nn.Module):
         return F.log_softmax(y, dim=1), x, z
 
 
-from torch.autograd import Function
 
 
 class ReverseLayerF(Function):
@@ -146,9 +147,17 @@ class ReverseLayerF(Function):
         return output, None
 
 class CIDA_Images_CNN_Model(nn.Module):
-    def __init__(self,
-        num_output_classes):
+    def __init__(
+            self,
+            num_output_classes,
+            label_loss_object,
+            domain_loss_object,
+            learning_rate
+        ):
         super(CIDA_Images_CNN_Model, self).__init__()
+
+        self.label_loss_object = label_loss_object
+        self.domain_loss_object = domain_loss_object
 
         nz = 100
         dim_domain = 1
@@ -157,6 +166,15 @@ class CIDA_Images_CNN_Model(nn.Module):
         self.init_weight(self.netE)
         self.netD = DiscConv(nz, nout=dim_domain)
 
+        # self.optimizer_G = torch.optim.Adam(self.netE.parameters(), lr=learning_rate, betas=(opt.beta1, 0.999), weight_decay=opt.weight_decay)
+        # self.optimizer_D = torch.optim.Adam(self.netD.parameters(), lr=learning_rate, betas=(opt.beta1, 0.999), weight_decay=opt.weight_decay)
+        self.optimizer_G = torch.optim.Adam(self.netE.parameters(), lr=learning_rate)
+        self.optimizer_D = torch.optim.Adam(self.netD.parameters(), lr=learning_rate)
+        self.lr_scheduler_G = lr_scheduler.ExponentialLR(optimizer=self.optimizer_G, gamma=0.5 ** (1 / 50))
+        self.lr_scheduler_D = lr_scheduler.ExponentialLR(optimizer=self.optimizer_D, gamma=0.5 ** (1 / 50))
+
+        # Cargo cult
+        self.lr_schedulers = [self.lr_scheduler_G, self.lr_scheduler_D]
     def init_weight(self, net=None):
         if net is None:
             net = self
@@ -166,17 +184,60 @@ class CIDA_Images_CNN_Model(nn.Module):
                 nn.init.constant_(m.bias, val=0)
 
 
-    def forward(self, x, t, alpha):
+    def forward(self, x, u):
+        y_hat, _, z = self.netE(x,u)
+        u_hat = self.netD(z)
+        u_hat = u_hat.reshape(-1)
 
-        # print(x)
-        # print(t)
-        # print(alpha)
+        return y_hat, u_hat
 
-        y_hat, _, z = self.netE(x,t)
+    def set_requires_grad(self, nets, requires_grad=False):
+        if not isinstance(nets, list):
+            nets = [nets]
+        for net in nets:
+            if net is not None:
+                for param in net.parameters():
+                    param.requires_grad = requires_grad
 
-        reverse_z = ReverseLayerF.apply(z, alpha)
+    def learn(self, x,y,u, alpha, domain_only:bool):
+        """
+        returns a dict of
+        {
+            label_loss:float, # if domain_only==False
+            domain_loss:float
+        }
+        """
 
-        t_hat = self.netD(reverse_z)
-        t_hat = t_hat.reshape(-1)
+        # Do domain's loss first, it is straight forward
+        y_hat, u_hat = self.forward(x,u)
+        descriminator_loss = self.domain_loss_object(u_hat, u)
+        self.set_requires_grad(self.netD, True)
+        self.optimizer_D.zero_grad()
+        descriminator_loss.backward()
+        self.optimizer_D.step()
 
-        return y_hat, t_hat
+        # Do encoder and potentially predictor. Note the negation of the descriminator loss
+        y_hat, u_hat = self.forward(x,u) # Yeah it's dumb but I can't find an easy way to train the two nets separately without this
+        encoder_loss = - alpha * self.domain_loss_object(u_hat, u)
+
+
+        if not domain_only:
+            label_loss = self.label_loss_object(y_hat, y)
+            encoder_loss += label_loss
+
+        # TODO: Disable Encoder Learning
+        self.set_requires_grad(self.netD, False)
+        self.optimizer_G.zero_grad()
+        encoder_loss.backward()
+        self.optimizer_G.step()
+
+
+        if domain_only:
+            return {
+                "domain_loss": descriminator_loss
+            }
+        else:
+            return {
+                "domain_loss": descriminator_loss,
+                "label_loss": label_loss
+            }
